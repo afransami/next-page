de7215396a84be87d0fa746ad337c96664dd527c
afransami <afran.sami007@gmail.com>
2023-04-06 14:43:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div className='bg-green-600 font-sans font-bold'>Hello world!</div>
+    </div>
+  );
+};
+
+export default App;
